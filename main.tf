807170ab7b2efc0b36afd3873efa82fd4e7b12e0
John Rey Tanquinco <johnreytanquinco@gmail.com>
2021-10-19 12:45:40 +0800

    Working web server in AWS

--- main.tf
+++ main.tf
@@ -3,10 +3,28 @@ provider "aws" {
 }
 
 resource "aws_instance" "my_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                   = "ami-0c55b159cbfafe1f0"
+  instance_type         = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World" > index.html
+              nohup busybox httpd -f -p 8080 &
+              EOF
 
   tags = {
     Name = "terraform-example"
+  }
+}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 }
