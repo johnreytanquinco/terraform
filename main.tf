5549bb6ce9cdf7c30dc22ed9145cc72cfa197cf7
John Rey Tanquinco <johnreytanquinco@gmail.com>
2021-10-19 15:50:04 +0800

    Finishing code but with unhealthy status

--- main.tf
+++ main.tf
@@ -2,15 +2,15 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "my_instance" {
-  ami                   = "ami-0c55b159cbfafe1f0"
-  instance_type         = "t2.micro"
+resource "aws_instance" "example" {
+  ami                   = var.image_id
+  instance_type         = var.instance_type
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p {$var.server_port} &
               EOF
 
   tags = {
@@ -22,9 +22,150 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = var.server_port
+    to_port     = var.server_port
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+}
+
+resource "aws_launch_configuration" "example" {
+  image_id        = var.image_id
+  instance_type   = var.instance_type
+  security_groups = [aws_security_group.instance.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, WORLD" > index.html
+              nohup busybox httpd -f -p {$var.server_port} &
+              EOF
+  # Required when using a LC with an ASG
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  target_group_arns = [aws_lb_target_group.asg.arn]
+  health_check_type = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_lb" "example" {
+  name                = "terraform-asg-example"
+  load_balancer_type  = "application"
+  subnets             = data.aws_subnet_ids.default.ids
+  security_groups     = [aws_security_group.alb.id]
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.example.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type  = "text/plain"
+      message_body  = "404: page not found"
+      status_code   = 404
+    }
+  }
+}
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  # Allow inbound HTTP requests
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all outbound requests
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks =["0.0.0.0/0"]
+
+  }
+}
+
+resource "aws_lb_target_group" "asg" {
+  name      = "terraform-asg-example"
+  port      = var.server_port
+  protocol  = "HTTP"
+  vpc_id    = data.aws_vpc.default.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 15
+    timeout             = 3
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+}
+
+resource "aws_lb_listener_rule" "asg" {
+  listener_arn  = aws_lb_listener.http.arn
+  priority      = 100
+
+  condition {
+    path_pattern {
+      values = ["*"]
+    }
+  }
+
+  action {
+    type              = "forward"
+    target_group_arn  = aws_lb_target_group.asg.arn
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_subnet_ids" "default" {
+  vpc_id  = data.aws_vpc.default.id
+}
+
+variable "image_id" {
+  description = "The id of the machine image (AMI) to use for the server."
+  type        = string
+  default     = "ami-0c55b159cbfafe1f0"
+}
+
+variable "instance_type" {
+  description = "The instance type of the machine image (AMI) to use for the server."
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  type        = number
+  default     = 8080
+}
+
+output "alb_dns_name" {
+  value        = aws_lb.example.dns_name
+  description  = "The domain name of the loadbalancer"
 }
